e87d07c5a356e2b1445021d7778f2d2e2d81fe24
Daniël Geerts <g.daniel.geerts@gmail.com>
2025-08-01 13:43:12 +0200

    Add particles background

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,34 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgxParticlesModule } from '@tsparticles/angular';
+import { Engine } from '@tsparticles/engine';
+import { loadLinksPreset } from '@tsparticles/preset-links';
 import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  template: `
-    <main>
-      <nav>
-        <a *ngFor="let route of navRoutes"
-          [routerLink]="route.path"
-          routerLinkActive="active"
-          [routerLinkActiveOptions]="{ exact: true }">
-          {{ route.title }}
-        </a>
-      </nav>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
-  styleUrl: './app.scss'
+  imports: [CommonModule, RouterModule, NgxParticlesModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('devdaniels-portfolio');
   protected readonly routes = routes;
   navRoutes = routes.filter(r => r.title);
+
+  id = 'tsparticles';
+
+  particlesOptions = {
+    preset: 'links',
+    background: {
+      color: {
+        value: 'black',
+      },
+    },
+  };
+
+  async particlesInit(engine: Engine): Promise<void> {
+    await loadLinksPreset(engine);
+  }
 }
