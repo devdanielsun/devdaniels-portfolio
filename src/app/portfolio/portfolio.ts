32e5ce9c9c76c6a43cacc3f370187037dc68caf7
devdanielsun <g.daniel.geerts@gmail.com>
2025-10-26 00:04:41 +0200

    Add best practice for implementing icons

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../modules/container-component/container-component";
 import { Router } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { 
+  diAzureOriginal,
+  diCsharpOriginal,
+  diGitOriginal,
+  diAngularOriginal,
+  diReactOriginal,
+  diNodejsOriginal,
+  diGithubOriginal } from '@ng-icons/devicon/original';
+import { faSolidIdCard } from '@ng-icons/font-awesome/solid';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [ContainerComponent],
+  imports: [
+    ContainerComponent,
+    FontAwesomeModule,
+    MatGridListModule,
+    NgIconComponent
+  ],
+  providers: [
+    provideIcons({
+      diAzureOriginal,
+      diCsharpOriginal,
+      diGitOriginal,
+      faSolidIdCard,
+      diAngularOriginal,
+      diReactOriginal,
+      diNodejsOriginal,
+      diGithubOriginal
+    })
+  ],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss'
 })
 export class Portfolio {
-
   constructor(private router: Router) {}
 
   goToProject(projectUri: string) {
