fcef9be42f1c269709d35833714ea80e240285b1
devdanielsun <g.daniel.geerts@gmail.com>
2026-01-10 16:07:23 +0100

    Update Codeerts article; and update article parent style

--- src/app/articles/codeerts/codeerts.article.ts
+++ src/app/articles/codeerts/codeerts.article.ts
@@ -3,8 +3,8 @@ import { Article } from '../../models/article.model';
 
 export const CODEERTS_ARTICLE: Article = {
   title: 'Codeerts',
-  shortDescription: 'Tijdens mijn studententijd was ik freelance developer onder de naam...',
-  date: '2020 t/m 2022',
+  shortDescription: 'Tijdens mijn studententijd ben ik als freelance developer actief geweest onder de naam Codeerts, van 2019 tot 2021. Mijn verantwoordelijkheden omvatten het ontwikkelen en onderhouden van websites en webshops voor verschillende klanten.',
+  date: '2019 t/m 2021',
   category: ['Projects', 'Freelance'],
   featuredImage: {
     altText: 'Codeerts Logo',
