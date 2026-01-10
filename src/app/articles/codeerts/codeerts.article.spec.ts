d6c2a906630a78b492dd358de6d233dd76c42fcb
devdanielsun <g.daniel.geerts@gmail.com>
2026-01-10 15:00:55 +0100

    Fix article routing links

---
+++ src/app/articles/codeerts/codeerts.article.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Codeerts } from './codeerts.article';
+
+describe('TerraformCicdPipeline', () => {
+  let component: Codeerts;
+  let fixture: ComponentFixture<Codeerts>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Codeerts]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Codeerts);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
